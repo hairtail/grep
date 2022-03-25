ef17782b519e316d822222e9df9a6fa105109b0b
hairtail <zhaoxinlong@web3.com>
2022-03-25 23:09:57 +0800

    FEAT: init commmit, upload file

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+use std::fs;
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let content =  fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "safe, fast, productive.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, content))
+    }
+}
